07f13071add2875859ffbc80a4b4a6a3d304301e
Paul Fries <pffries@amazon.com>
2022-02-26 15:08:58 -0700

    Initial Commit

---
+++ main.tf
@@ -0,0 +1,10 @@
+resource "aws_cloudformation_stack" "datalake" {
+  name = "datalake-deploy-stack"
+  capabilities = ["CAPABILITY_AUTO_EXPAND", "CAPABILITY_IAM", "CAPABILITY_NAMED_IAM"]
+  parameters = {
+   AdministratorName = "Paul Fries"
+   AdministratorEmail = "pffries@amazon.com" 
+   CognitoDomain = "test"
+  }
+  template_url = "https://s3.amazonaws.com/solutions-reference/data-lake-solution/latest/data-lake-deploy.template"
+}
