436c613b267c153a24e33ba6d41554ef7c171a4d
Paul Fries <pffries@amazon.com>
2022-02-27 15:46:14 -0700

    Fixing null condition

--- main.tf
+++ main.tf
@@ -2,15 +2,15 @@ resource "aws_cloudformation_stack" "datalake" {
   name         = "datalake-deploy-stack"
   capabilities = ["CAPABILITY_IAM", "CAPABILITY_NAMED_IAM"]
   parameters = {
-    AdministratorName  = var.DeployFederated ? null : var.AdministratorName
-    AdministratorEmail = var.DeployFederated ? null : var.AdministratorEmail
+    AdministratorName  = var.DeployFederated ? false : var.AdministratorName
+    AdministratorEmail = var.DeployFederated ? false : var.AdministratorEmail
     CognitoDomain      = var.CognitoDomain
-    AdFsHostname       = var.DeployFederated ? var.AdFsHostname : null
+    AdFsHostname       = var.DeployFederated ? var.AdFsHostname : false
   }
   template_url = var.DeployFederated ? var.FederatedTemplate : var.NonFederatedTemplate
 
 }
 output "sso_instruction" {
-  value       = var.DeployFederated ? "You've Deployed the federated stack" : null
+  value       = var.DeployFederated ? "You've Deployed the federated stack" : false
   description = "Please complete the manual SSO configuration with the instructions found here: https://docs.aws.amazon.com/solutions/latest/data-lake-solution/appendix-a.html"
 }
