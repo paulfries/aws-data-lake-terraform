225d54d090d17a222b1101bc1d57ec498d9d47d5
Paul Fries <pffries@amazon.com>
2022-02-28 08:11:44 -0700

    Adjustments for TF Cloud

--- main.tf
+++ main.tf
@@ -2,15 +2,15 @@ resource "aws_cloudformation_stack" "datalake" {
   name         = "datalake-deploy-stack"
   capabilities = ["CAPABILITY_IAM", "CAPABILITY_NAMED_IAM"]
   parameters = {
-    AdministratorName  = var.DeployFederated ? false : var.AdministratorName
-    AdministratorEmail = var.DeployFederated ? false : var.AdministratorEmail
+    AdministratorName  = var.DeployFederated ? null : var.AdministratorName
+    AdministratorEmail = var.DeployFederated ? null : var.AdministratorEmail
     CognitoDomain      = var.CognitoDomain
-    AdFsHostname       = var.DeployFederated ? var.AdFsHostname : false
+    AdFsHostname       = var.DeployFederated ? var.AdFsHostname : null
   }
   template_url = var.DeployFederated ? var.FederatedTemplate : var.NonFederatedTemplate
 
 }
 output "sso_instruction" {
-  value       = var.DeployFederated ? "You've Deployed the federated stack" : false
+  value       = var.DeployFederated ? "You've Deployed the federated stack" : null
   description = "Please complete the manual SSO configuration with the instructions found here: https://docs.aws.amazon.com/solutions/latest/data-lake-solution/appendix-a.html"
 }
